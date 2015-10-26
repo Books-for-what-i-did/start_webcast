6676b5f4b9574f71223c0a88bcb6caa1d5c99507
wurikiji <wurikiji@gmail.com>
2015-10-26 12:27:05 +0900

    Add views for socialgraph
    * following
    * follower
    * mutual followers

--- socialgraph/util.py
+++ socialgraph/util.py
@@ -9,13 +9,13 @@ def get_people_user_follows(user):
 
 
 def get_people_following_user(user):
-	ulink = UserLink.objects.filter(from_user=user).values('from_user')
+	ulink = UserLink.objects.filter(to_user=user).values('from_user')
 	return User.objects.filter(id__in=[i['from_user'] for i in ulink])
 
 
-def get_muttual_followers(user):
+def get_mutual_followers(user):
 	follower= UserLink.objects.filter(from_user=user).values('to_user')
-	following = UserLink.objects.filter(from_user=user).values('from_user')
+	following = UserLink.objects.filter(to_user=user).values('from_user')
 	follows_set = set([i['to_user'] for i in follower])
 	following_set = set([i['from_user'] for i in following])
 	return User.objects.filter(id__in=follows_set.intersection(following_set))
